09ab450170caa3cfcc8bbf29bea567ca842df621
GradientBooster <144033691+aditya0589@users.noreply.github.com>
2026-05-11 10:54:15 +0530

    Add SQL queries for Middle East, North Africa, and South Asia
    Added queries for Middle East and North Africa and South Asia, including total attacks and average casualty rates.

--- sql/global_hotspots.sql
+++ sql/global_hotspots.sql
@@ -50,3 +50,51 @@ GROUP BY country
 ORDER BY total_killed DESC
 LIMIT 10;
 
+--> Middle East and North Africa
+--> top 10 countries with highest attacks and casualties
+SELECT
+    country,
+    COUNT(*) AS attacks,
+    SUM(COALESCE(casualties, 0)) AS total_killed
+FROM terrorism_final
+WHERE region = 'Middle East & North Africa'
+GROUP BY country
+ORDER BY attacks DESC
+LIMIT 10;
+
+--> Average casualty rates
+SELECT
+    country,
+    COUNT(*) AS attacks,
+    SUM(COALESCE(casualties, 0)) AS total_killed,
+    ROUND((total_killed / attacks), 2) as avg_killed
+FROM terrorism_final
+WHERE region = 'Middle East & North Africa'
+GROUP BY country
+ORDER BY attacks DESC
+LIMIT 10;
+
+ --> South Asia
+--> countries with the highest number of attacks
+SELECT
+    country,
+    COUNT(*) AS attacks
+FROM terrorism_final
+WHERE region = "South Asia"
+GROUP BY country
+ORDER BY attacks DESC
+LIMIT 10;
+
+--> average casualty rates of countries
+SELECT
+    country,
+    COUNT(*) AS attacks, 
+    SUM(COALESCE(casualties, 0)) AS total_killed,
+    ROUND((total_killed / attacks), 2) as avg_killed
+FROM terrorism_final
+WHERE region = 'South Asia'
+GROUP BY country
+ORDER BY attacks DESC
+LIMIT 10;
+
+
