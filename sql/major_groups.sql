d17488c0263eb9656987a227ba450d3ba5bc3eb3
GradientBooster <yraditya895@gmail.com>
2026-07-19 10:41:18 +0530

    Add SQL queries for various regions in major_groups.sql

--- sql/major_groups.sql
+++ sql/major_groups.sql
@@ -19,3 +19,57 @@ WHERE region = "Middle East & North Africa" AND gname != "Unknown"
 GROUP BY gname
 ORDER BY attacks DESC
 LIMIT 10;
+
+--> East Asia
+
+SELECT
+    gname,
+    COUNT(*) AS attacks
+FROM terrorism_final
+WHERE region = "East Asia" AND gname != "Unknown"
+GROUP BY gname
+ORDER BY attacks DESC
+LIMIT 10;
+
+--> South Asia
+SELECT
+    gname,
+    COUNT(*) AS attacks
+FROM terrorism_final
+WHERE region = "South Asia" AND gname != "Unknown"
+GROUP BY gname
+ORDER BY attacks DESC
+LIMIT 10;
+
+--> North America
+
+SELECT
+    gname,
+    COUNT(*) AS attacks
+FROM terrorism_final
+WHERE region = "North America" AND gname != "Unknown"
+GROUP BY gname
+ORDER BY attacks DESC
+LIMIT 10
+
+--> subsaharan africa
+SELECT
+    gname,
+    COUNT(*) AS attacks
+FROM terrorism_final
+WHERE region = "Sub-Saharan Africa" AND gname != "Unknown"
+GROUP BY gname
+ORDER BY attacks DESC
+LIMIT 10;
+
+--> central asia
+
+SELECT
+    gname,
+    COUNT(*) AS attacks
+FROM terrorism_final
+WHERE region = "Central Asia" AND gname != "Unknown"
+GROUP BY gname
+ORDER BY attacks DESC
+LIMIT 10;
+-->
